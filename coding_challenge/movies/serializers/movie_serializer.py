4298b04d8f3dbaab7e708699d20ec4b8588256c4
Gary Duncan <gduncan@bettercollective.com>
2024-04-24 10:50:54 -0500

    Updated serializers

--- coding_challenge/movies/serializers/movie_serializer.py
+++ coding_challenge/movies/serializers/movie_serializer.py
@@ -1,14 +1,32 @@
 from rest_framework import serializers
+from django.db.models import Avg
 
 from movies.models import Movie
-
+from movies.serializers.review_serializer import ReviewSerializer
 
 class MovieSerializer(serializers.ModelSerializer):
+    avg_rating = serializers.SerializerMethodField()
+    reviewers = ReviewSerializer(many=True, read_only=True)
+    runtime_formatted = serializers.SerializerMethodField()
+
     class Meta:
         model = Movie
         fields = (
             "id",
             "title",
-            "runtime",
             "release_date",
+            "runtime",
+            "runtime_formatted",
+            "reviewers",
+            "avg_rating",
         )
+
+    def get_avg_rating(self, obj):
+        average = obj.reviewers.aggregate(Avg('rating')).get('rating__avg')
+        return average if average is not None else "No reviews"
+
+    def get_runtime_formatted(self, obj):
+        hours = obj.runtime // 60
+        minutes = obj.runtime % 60
+        return f"{hours}:{minutes:02}"
+        
---
+++ coding_challenge/movies/serializers/review_serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from movies.models import Review
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ("name", "rating")
